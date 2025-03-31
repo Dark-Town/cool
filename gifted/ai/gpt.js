cfaa03a4a6031bf2ae4191fcb2864a463507352e
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-03-31 14:32:23 +0200

    Update gpt.js

--- gifted/ai/gpt.js
+++ gifted/ai/gpt.js
@@ -14,7 +14,7 @@ let Giftedd = async (m, { Gifted, text, fetchJson }) => {
     ];
 
     try {
-        const aiResponse = await fetchJson(`https://ab-text-voice.abrahamdw882.workers.dev/api/tts?model=tts-english3&text=Hi%20there!%20It%27s%20nice%20to%20meet%20you.%20How%20are%20you%20doing%20today%3F%20Is%20there%20something%20on%20your%20mind%20that%20you%27d%20like%20to%20talk%20about%20or%20ask%20me%3F%20I%27m%20here%20to%20help!%20%F0%9F%98%8A`);
+        const aiResponse = await fetchJson(`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`);
         const giftedResponse = aiResponse.result;
 
         Gifted.reply( { text: giftedResponse}, giftedButtons, m);

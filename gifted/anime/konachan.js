17422942fbef89dbcf553be163068ef2489df4f4
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-03-31 15:20:03 +0200

    Update konachan.js

--- gifted/anime/konachan.js
+++ gifted/anime/konachan.js
@@ -3,7 +3,7 @@ const axios = require('axios'),
 
 async function giftedKonachan() {
     try {
-        let { giftedData } = await axios.get('https://konachan.net/post?tags=order%3Arandom')
+        let { giftedData } = await axios.get('https://apis.davidcyriltech.my.id/random/quotes')
         let $ = cheerio.load(giftedData)
         let img = []
         $('#post-list-posts a.directlink.largeimg').each((index, element) => {
